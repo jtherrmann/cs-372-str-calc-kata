07c4cda24deaf019ed82a80b6302379528b019e5
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-23 19:52:26 -0900

    Refactor into several smaller functions.

--- str_calc.py
+++ str_calc.py
@@ -1,27 +1,30 @@
-def str_calc(s):
-    return sum(map(to_num, num_strs(s)))
+def str_calc(inpt):
+    return sum(map(to_num, num_strs(inpt)))
 
 
-def num_strs(s):
-    s, delim = get_str_delim_pair(s)
-    return s.replace(delim, " ").split()
-
-
-def get_str_delim_pair(s):
-    lines = s.split("\n")
-    if lines[0][:2] == "//":
-        delim = lines[0][2:]
-        if len(delim) > 1:
-            assert delim[0] == "[" and delim[-1] == "]"
-            delim = delim[1:-1]
-        s = "".join(lines[1:])
-    else:
-        delim = ","
-    return s, delim
-
-
-def to_num(s):
-    n = int(s)
-    if n < 0:
+def to_num(num_str):
+    num = int(num_str)
+    if num < 0:
         raise ValueError()
-    return n if n <= 1000 else 0
+    return num if num <= 1000 else 0
+
+
+def num_strs(inpt):
+    delim, rest = extract_delim(inpt)
+    return rest.replace(delim, " ").split()
+
+
+def extract_delim(inpt):
+    lines = inpt.split("\n")
+    if lines[0][:2] == "//":
+        return parse_delim(lines[0][2:]), "".join(lines[1:])
+    return ",", inpt
+
+
+def parse_delim(delim):
+    return parse_multi_char_delim(delim) if len(delim) > 1 else delim
+
+
+def parse_multi_char_delim(delim):
+    assert delim[0] == "[" and delim[-1] == "]"
+    return delim[1:-1]
