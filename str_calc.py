c08772a0be12bff61cd1eea5b909b4210227f49b
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-23 19:17:05 -0900

    Refactor str_calc to be more concise.

--- str_calc.py
+++ str_calc.py
@@ -1,26 +1,16 @@
 def str_calc(s):
-    if s == "":
-        result = 0
-    elif "," in s:
-        nums = s.split(",")
-        assert len(nums) in (2, 3)
-        result = sum(map(to_num, nums))
-    elif "\n" in s:
-        lines = s.split("\n")
-        if lines[0][:2] == "//":
-            assert len(lines) == 2
-            delim = lines[0][2:]
-            if len(delim) > 1:
-                assert delim[0] == "[" and delim[-1] == "]"
-                delim = delim[1:-1]
-            nums = lines[1].split(delim)
-        else:
-            nums = lines
-        assert len(nums) in (2, 3)
-        result = sum(map(to_num, nums))
+    lines = s.split("\n")
+    if lines[0][:2] == "//":
+        delim = lines[0][2:]
+        if len(delim) > 1:
+            assert delim[0] == "[" and delim[-1] == "]"
+            delim = delim[1:-1]
+        s = "".join(lines[1:])
     else:
-        result = to_num(s)
-    return result
+        delim = ","
+
+    nums = s.replace(delim, " ").split()
+    return sum(map(to_num, nums))
 
 
 def to_num(s):
