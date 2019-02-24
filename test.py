564a47d75426b536658ea214eacbc5d0efc4cce2
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-23 17:53:14 -0900

    Add test_single_num.

--- test.py
+++ test.py
@@ -8,6 +8,11 @@ class StrCalcTestCase(unittest.TestCase):
     def test_empty_str(self):
         self.assertEqual(str_calc(""), 0)
 
+    def test_single_num(self):
+        self.assertEqual(str_calc("0"), 0)
+        self.assertEqual(str_calc("123"), 123)
+        self.assertEqual(str_calc("-123"), -123)
+
 
 if __name__ == "__main__":
     unittest.main()
