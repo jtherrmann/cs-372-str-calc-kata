f14d590c4a27433de6ff0d5a862fa2df0c92ffa3
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-23 18:27:02 -0900

    Pass test_negative_nums.

--- str_calc.py
+++ str_calc.py
@@ -5,12 +5,19 @@ def str_calc(s):
         assert "\n" not in s
         nums = s.split(",")
         assert len(nums) in (2, 3)
-        result = sum(map(int, nums))
+        result = sum(map(to_num, nums))
     elif "\n" in s:
         assert "," not in s
         nums = s.split("\n")
         assert len(nums) in (2, 3)
-        result = sum(map(int, nums))
+        result = sum(map(to_num, nums))
     else:
-        result = int(s)
+        result = to_num(s)
     return result
+
+
+def to_num(s):
+    n = int(s)
+    if n < 0:
+        raise ValueError()
+    return n
