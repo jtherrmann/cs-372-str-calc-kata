09ab6f9f336dabb13a26d424589ad3f3f5b484d8
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-23 18:13:13 -0900

    Pass test_two_nums_newline.

--- str_calc.py
+++ str_calc.py
@@ -1,10 +1,16 @@
 def str_calc(s):
     if s == "":
         result = 0
-    elif "," not in s:
-        result = int(s)
-    else:
+    elif "," in s:
+        assert "\n" not in s
         nums = s.split(",")
         assert len(nums) == 2
         result = sum(map(int, nums))
+    elif "\n" in s:
+        assert "," not in s
+        nums = s.split("\n")
+        assert len(nums) == 2
+        result = sum(map(int, nums))
+    else:
+        result = int(s)
     return result
